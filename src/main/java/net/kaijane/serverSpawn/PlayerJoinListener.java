baf889be725e80078e4edca1d16e3ed61ef0ca9a
Kai Jane <kaijanedev@icloud.com>
2025-01-17 17:51:15 +0800

    Initial commit

---
+++ src/main/java/net/kaijane/serverSpawn/PlayerJoinListener.java
@@ -0,0 +1,4 @@
+package net.kaijane.serverSpawn;
+
+public class PlayerJoinListener {
+}
---
+++ src/main/java/net/kaijane/serverSpawn/ServerSpawn.java
@@ -0,0 +1,17 @@
+package net.kaijane.serverSpawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerSpawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
